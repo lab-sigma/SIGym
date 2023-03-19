3cb430084e97f0520e71d2f190ed9bac8f54a986
LuciEN <33297246+lucien386@users.noreply.github.com>
2023-03-19 13:35:49 -0500

    Update main.py

--- Example/main.py
+++ Example/main.py
@@ -6,6 +6,7 @@ from General_Stackelberg.dynamic_stackelberg import dyse, SSE
 from General_Stackelberg.dynamic_stackelberg import utils
 import utils
 from sigym import Follower, Platform
+import tkinter
 
 # def test_without_follower_class():
 #     defender_mode_dict = {
@@ -80,85 +81,95 @@ from sigym import Follower, Platform
 #             rgt += (u_sse*T - u_t)/T
 #         print("The averaged regret you get over {} trials is {}".format(trial, rgt/trial))
 
+defender_mode_dict = {
+    '1': "random",
+    '2': 'MWU'
+}
+
+attacker_mode_dict = {
+    '1': "random",
+    '2': 'best_response'
+}
+
+defender_mode = '1'
+attacker_mode = '2'
+T = 10
+m, n = 3, 3
+eps = 0.01
+trial = 5
+imgheight = 1000
+imgwidth = 1000
+base = 50, 50
+base2 = imgwidth/2+50, 50
+squareL = 70
+i_res = []
+j_res = []
+update_cnt = 0
+
+# init tk
+root = tkinter.Tk()
+myCanvas = tkinter.Canvas(root, bg="white", height=imgheight, width=imgwidth)
+
+def update_win():
+    global update_cnt, root
+    if update_cnt == len(i_res):
+        return
+    for cnt in range(m):
+        color = 'green' if (cnt == i_res[update_cnt]) else 'white'
+        myCanvas.create_rectangle(base[0]+cnt*squareL,base[1]+squareL*update_cnt,base[0]+cnt*squareL+squareL,base[1]+squareL*update_cnt+squareL,fill=color)
+    for cnt in range(n):
+        color = 'red' if (cnt == j_res[update_cnt]) else 'white'
+        myCanvas.create_rectangle(base2[0]+cnt*squareL,base2[1]+squareL*update_cnt,base2[0]+cnt*squareL+squareL,base2[1]+squareL*update_cnt+squareL,fill=color)
+    myCanvas.pack()
+    update_cnt+=1
+    root.after(100, update_win)
+
 def main():
+    global i_res, j_res, root, myCanvas, update_cnt
+    rgt = 0.0
+    for tr in range(trial):
+        update_cnt = 0
+        i_res = []
+        j_res = []
+        R = np.loadtxt("random_instance/R_{}.txt".format(tr))
+        C = np.loadtxt("random_instance/C_{}.txt".format(0))
+        if tr != 0:
+            # re-init tk
+            root = tkinter.Tk()
+            myCanvas = tkinter.Canvas(root, bg="white", height=imgheight, width=imgwidth)
 
-    defender_mode_dict = {
-        '1': "random",
-        '2': 'MWU'
-    }
-
-    attacker_mode_dict = {
-        '1': "random",
-        '2': 'best_response'
-    }
-
-    defender_mode = '1'
-    attacker_mode = '2'
-    T = 10
-    m, n = 3, 3
-    eps = 0.01
-    trial = 10
-
-    if defender_mode == '2':
-        rgt = 0.0
-        for tr in range(trial):
-            # Loading  the utility matrices of the leader and the follower 
-            R = np.loadtxt("random_instance/R_{}.txt".format(tr))
-            C = np.loadtxt("random_instance/C_{}.txt".format(0))
-
-            agent = Follower(utility_matrix=C, behavior_mode=attacker_mode_dict[attacker_mode])  # Instantiate a follower class using the utility matrix and the behavior model
-            env = Platform(R, C)
-            u_sse = env.compute_SSE()
-
-            x = [1.0/m for _ in range(m)]
-            for t in range(T):
-                i_t, j_t = env.step(x, agent) # the platform takes a step based on the leader strategy and the follower model
-                env.cum_u += R[i_t][j_t]
-                
-                print("At round " + str(t) + ", the leader plays action " + str(i_t) + " according to mixed strategy " + str(x) + ", the follower responses by " + str(j_t) + ".")
-                print("The leader utility at current round is {}, and the cumulative leader utility until current round is {}.".format(R[i_t][j_t], env.cum_u))
-                print("==============================="*5)
-                
-                ###################################################
-                # the user updates the leader strategy (e.g. MWU) #
-                ###################################################
-                reward_t = R[:, j_t]
-                x = utils.mwu_update(x=x, reward=reward_t, eps=eps)
-
-            rgt += (u_sse*T - env.cum_u)/T
-        print("The averaged regret you get over {} trials is {}".format(trial, rgt/trial))
-    
-    else:
-        rgt = 0.0
-        for tr in range(trial):
-            R = np.loadtxt("random_instance/R_{}.txt".format(tr))
-            C = np.loadtxt("random_instance/C_{}.txt".format(0))
-            
-            agent = Follower(utility_matrix=C, behavior_mode=attacker_mode_dict[attacker_mode])  # Instantiate a follower class using the utility matrix and the behavior model
-            env = Platform(R, C)
-            u_sse = env.compute_SSE()
-
+        agent = Follower(utility_matrix=C, behavior_mode=attacker_mode_dict[attacker_mode])  # Instantiate a follower class using the utility matrix and the behavior model
+        env = Platform(R, C)
+        u_sse = env.compute_SSE()
+        if defender_mode == '1':
             x = [np.random.rand() for i in range(m)]
             temp = sum(x)
             x = [i/temp for i in x]
-            
-            for t in range(T):
-                i_t, j_t = env.step(x, agent) # the platform takes a step based on the leader strategy and the follower model
-                env.cum_u += R[i_t][j_t]
-
-                print("At round " + str(t) + ", the leader plays action " + str(i_t) + " according to mixed strategy " + str(x) + ", the follower responses by " + str(j_t) + ".")
-                print("The leader utility at current round is {}, and the cumulative leader utility until current round is {}.".format(R[i_t][j_t], env.cum_u))
-                print("==============================="*5)
-
-                ############################################################
-                # the user updates the leader strategy (e.g. radom update) #
-                ############################################################
+        else:
+            x = [1.0/m for _ in range(m)]
+        for t in range(T):
+            i_t, j_t = env.step(x, agent) # the platform takes a step based on the leader strategy and the follower model
+            env.cum_u += R[i_t][j_t]
+            i_res.append(i_t)
+            j_res.append(j_t)
+            print("At round " + str(t) + ", the leader plays action " + str(i_t) + " according to " + defender_mode_dict[defender_mode] + str(x) + ", the follower responses by " + str(j_t) + ".")
+            print("The leader utility at current round is {}, and the cumulative leader utility until current round is {}.".format(R[i_t][j_t], env.cum_u))
+            print("==============================="*5)
+            ############################################################
+            # the user updates the leader strategy (e.g. random update) #
+            ############################################################
+            if defender_mode == '1':
                 x = [np.random.rand() for i in range(m)]
                 temp = sum(x)
-                x = [i/temp for i in x]
-
-            rgt += (u_sse*T - env.cum_u)/T
-        print("The averaged regret you get over {} trials is {}".format(trial, rgt/trial))
+                x = [i/temp for i in x]     
+            else:
+                reward_t = R[:, j_t]
+                x = utils.mwu_update(x=x, reward=reward_t, eps=eps)
+        print(i_res, j_res)
+        update_win()
+        root.mainloop()
+        rgt += (u_sse*T - env.cum_u)/T
+    print("The averaged regret you get over {} trials is {}".format(trial, rgt/trial))
 
 if __name__ == '__main__':
     main()
