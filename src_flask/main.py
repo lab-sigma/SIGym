ca9870d90a7c93e5d0c7e5b9a44e1c9c06ef4c17
LuciEN <33297246+lucien386@users.noreply.github.com>
2023-04-10 13:12:21 -0500

    test

--- src_flask/main.py
+++ src_flask/main.py
@@ -1,10 +1,20 @@
 from flask import Flask
 from flask import Blueprint, current_app, g, json, render_template, request
-from src_sigym import gen
+#from src_sigym import gen
 bp = Blueprint("main", __name__)
+
+l_res = [0, 1, 2, 2, 1, 0, 1, 2, 1, 0]
+f_res = [0, 0, 0, 1, 1, 1, 2, 2, 2, 0]
+
+rgt_res = [0.4861321016343646, 0.9632937032687292, 1.4138241049030937, 1.8553840065374583, 2.270312708171823, 2.676270909806188, 3.0286642114405526, 3.3544263130749172, 3.6535572147092816, 3.9437176163436463]
+
+rgt = 3.9437176163436463
 
 @bp.route('/', methods=['POST', 'GET'])
 def index():
-    l_res, f_res, rgt, m, n= gen.main()
+    '''
+    l_res, f_res, rgt_res, rgt, m, n= gen.main()
     print(l_res, f_res, rgt)
     return render_template('base.html', l_res = l_res, f_res = f_res, rgt = rgt, m = m, n = n)
+    '''
+    return render_template('base.html', l_res = l_res, f_res = f_res, rgt_res = rgt_res, rgt = rgt, m = 3, n = 3)
--- src_sigym/gen.py
+++ src_sigym/gen.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd
 import time
 from tqdm import tqdm
+import sys, os
+ 
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 from src_sigym.General_Stackelberg.dynamic_stackelberg import dyse, SSE
 from src_sigym.General_Stackelberg.dynamic_stackelberg import utils as uG
@@ -25,7 +29,7 @@ attacker_mode = '2'
 T = 10
 m, n = 3, 3 #m defender row / n attacker colomn
 eps = 0.01
-trial = 1
+trial = 15
 imgheight = 600
 imgwidth = 600
 base = 50, 50
@@ -34,17 +38,24 @@ squareL = min((imgwidth - base[0]*2) / m, (imgheight - base[1]*2) / n )
 i_res = []
 j_res = []
 update_cnt = 0
-
+rgt = 0.0
 info = ''
 
 def main():
-    global i_res, j_res, update_cnt, info
-    rgt = 0.0
+    global i_res, j_res, update_cnt, info, rgt
+    cur_rgt = 0.0
     update_cnt = 0
     i_res = []
     j_res = []
-    R = np.loadtxt("src_sigym/random_instance/R_{}.txt".format(0))
-    C = np.loadtxt("src_sigym/random_instance/C_{}.txt".format(0))
+    rgt_res = []
+    try:
+        R = np.loadtxt("src_sigym/random_instance/R_{}.txt".format(0))
+        C = np.loadtxt("src_sigym/random_instance/C_{}.txt".format(0))
+    except:
+        R = np.loadtxt("random_instance/R_{}.txt".format(0))
+        C = np.loadtxt("random_instance/C_{}.txt".format(0))
+    else:
+        print("***Flask Env***")
     agent = Follower(utility_matrix=C, behavior_mode=attacker_mode_dict[attacker_mode])  # Instantiate a follower class using the utility matrix and the behavior model
     env = Platform(R, C)
     u_sse = env.compute_SSE()
@@ -74,9 +85,14 @@ def main():
             reward_t = R[:, j_t]
             x = utils.mwu_update(x=x, reward=reward_t, eps=eps)
         #print(i_res, j_res)
-        rgt += (u_sse*T - env.cum_u)/T 
-    print("The averaged regret you get over {} trial(s) is {}".format(trial, rgt/trial))
-    return i_res, j_res, rgt, m, n
+        cur_rgt += (u_sse*T - env.cum_u)/T
+        rgt_res.append(cur_rgt)
+    print("The regret for this trial is {}".format(cur_rgt))
+    print(i_res)
+    print(j_res)
+    print(rgt_res)
+    rgt+=cur_rgt
+    return i_res, j_res, rgt_res, rgt, m, n
 
 if __name__ == '__main__':
     main()
