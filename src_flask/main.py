865304b3a70b8b9d1874590b03d6c7d7818557c0
LuciEN <33297246+lucien386@users.noreply.github.com>
2023-04-19 01:10:01 -0500

    upd

--- src_flask/main.py
+++ src_flask/main.py
@@ -3,8 +3,8 @@ from flask import Blueprint, current_app, g, json, render_template, request
 #from src_sigym import gen
 bp = Blueprint("main", __name__)
 
-l_res = [0, 1, 2, 2, 1, 0, 1, 2, 1, 0]
-f_res = [0, 0, 0, 1, 1, 1, 2, 2, 2, 0]
+l_res = [0, 1, 2, 4, 1, 4, 1, 2, 1, 0]
+f_res = [0, 3, 4, 1, 2, 1, 2, 2, 3, 0]
 
 rgt_res = [0.4861321016343646, 0.9632937032687292, 1.4138241049030937, 1.8553840065374583, 2.270312708171823, 2.676270909806188, 3.0286642114405526, 3.3544263130749172, 3.6535572147092816, 3.9437176163436463]
 
@@ -17,4 +17,4 @@ def index():
     print(l_res, f_res, rgt)
     return render_template('base.html', l_res = l_res, f_res = f_res, rgt = rgt, m = m, n = n)
     '''
-    return render_template('base.html', l_res = l_res, f_res = f_res, rgt_res = rgt_res, rgt = rgt, m = 3, n = 3)
+    return render_template('base.html', l_res = l_res, f_res = f_res, rgt_res = rgt_res, rgt = rgt, m = 5, n = 5)
