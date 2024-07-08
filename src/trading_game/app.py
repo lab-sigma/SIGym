22f089d894ab2ffe7c15ddbcce2f3bbe4efb9484
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2024-07-08 06:05:07 -0500

    game intializatin works

--- src/trading_game/app.py
+++ src/trading_game/app.py
@@ -15,11 +15,11 @@ from .example_bots import IMCMMBot, HRTMMBot, GoldmanTakerBot, TwoSigmaTakerBot
 
 app = Flask(__name__)
 
-# Initialize game
+# Initialize default game variables
 mmTotal = 0
 takerTotal = 0
 
-# Set up players
+# Players and bots
 mm = IMCMMBot("IMC Trading", chips=100, maxspread=5)
 #mm = MM("player mm", chips=100, maxspread=5)
 #mm = HRTMMBot("Hudson River Trading", chips=100, maxspread=5)
@@ -28,65 +28,87 @@ taker = Taker("player taker", chips = 100)
 
 # Game settings
 numRounds = 6
-game = Game(mm,taker,numRounds)
-
-# Start game
-game.start_round()
-cardsRevealed = game.cards[:game.curRound]
-cardsHidden = game.cards[game.curRound:]
-bid, ask, contracts = game.req_market()
+timer = 60
+game = None
+gameActive = False
 
 @app.route("/trading-game", methods=("GET", "POST"))
 def trading_game():
-    global cardsRevealed 
-    global cardsHidden
     global mmTotal
     global takerTotal
-    global bid
-    global ask
-    global contracts
+    global mm
+    global taker
+    global numRounds
+    global timer
+    global game
+    global gameActive
 
+    cardsRevealed = cardsHidden = []
+    bid = ask = contracts = 0
+    
     if request.method == "POST":
-        # Process player action
-        action = request.form["submit_btn"].lower()
-        amount = int(request.form[action + "_amt"])
-        
-        print("action:{}".format(action))
-        print("amt:{}".format(amount))
-        
-        # Simulate this round
-        if game.curRound != numRounds:
+        if request.form["submit_btn"] == "Start Game":
+            """ Initialize game """
+
+            # Set up players and bots
+            mm = IMCMMBot("IMC Trading", chips=100, maxspread=5)
+            #mm = MM("player mm", chips=100, maxspread=5)
+            #mm = HRTMMBot("Hudson River Trading", chips=100, maxspread=5)
+            taker = Taker("player taker", chips = 100)
+            #taker = GoldmanTakerBot("GoldmanTakerBot", chips = 100)
+
+            # Game settings
+            numRounds = 6
+            game = Game(mm,taker,numRounds)
+
+            # Start game
+            game.start_round()
+            cardsRevealed = game.cards[:game.curRound]
+            cardsHidden = game.cards[game.curRound:]
+            bid, ask, contracts = game.req_market()
+
+            gameActive = True
+        else:
+            # Process player action
+            action = request.form["submit_btn"].lower()
+            amount = int(request.form[action + "_amt"])
             
-            game.req_taker(action, amount)
-
-            game.clear_book()
-            game.print_info()
-
-            game.end_round()
-
-            print(mm.name, "CHIPS:", mm.chips)
-            print(taker.name, "CHIPS:", taker.chips)
-
+            print("action:{}".format(action))
+            print("amt:{}".format(amount))
+            
+            # Simulate this round
             if game.curRound != numRounds:
-                game.start_round()
-                bid, ask, contracts = game.req_market()
-            else:
-                # End game after last round
-                game.end_game()
+                
+                game.req_taker(action, amount)
+
+                game.clear_book()
+                game.print_info()
+
+                game.end_round()
+
                 print(mm.name, "CHIPS:", mm.chips)
                 print(taker.name, "CHIPS:", taker.chips)
-                mmTotal += mm.chips 
-                takerTotal += taker.chips 
 
-                print("MARKET MAKER PROFIT", round(mmTotal - 100, 3))
-                print("TAKER PROFIT", round(takerTotal - 100, 3))
+                if game.curRound != numRounds:
+                    game.start_round()
+                    bid, ask, contracts = game.req_market()
+                else:
+                    # End game after last round
+                    game.end_game()
+                    print(mm.name, "CHIPS:", mm.chips)
+                    print(taker.name, "CHIPS:", taker.chips)
+                    mmTotal += mm.chips 
+                    takerTotal += taker.chips 
 
-        # Update cards display
-        cardsRevealed = game.cards[:game.curRound]
-        cardsHidden = game.cards[game.curRound:]
+                    print("MARKET MAKER PROFIT", round(mmTotal - 100, 3))
+                    print("TAKER PROFIT", round(takerTotal - 100, 3))
+
+            # Update cards display
+            cardsRevealed = game.cards[:game.curRound]
+            cardsHidden = game.cards[game.curRound:]
     
     return render_template('trading-game.html', 
-                           gameActive=True,
+                           gameActive=gameActive,
                            cardsRevealed=cardsRevealed, 
                            cardsHidden=cardsHidden, 
                            bid=bid, 
